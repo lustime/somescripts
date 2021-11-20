ec6f3a4459adf828f40a7bd1185ae2461696d477
lustime <2247868358@qq.com>
2021-11-20 19:07:17 +0800

    配置切换至toml

--- utils.py
+++ utils.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 
 import os
+import traceback
 
-import json5 as json
+import tomli
 
 import utils_env
 from utils_ver import print_ver
@@ -22,23 +23,22 @@ def get_data() -> dict:
     if DATA:
         return DATA
 
-    # check_config = os.getenv("CHECK_CONFIG")
-    # if not os.path.exists(check_config):
-    #     print(f"错误：环境变量 CHECK_CONFIG 指定的配置文件 {check_config} 不存在！")
-    #     exit(1)
-    # else:
-    check_config = utils_env.get_file_path(
-        "check.json5"
-    ) or utils_env.get_file_path("check.json")
-    if not check_config:
-        print("错误：未检查到签到配置文件，请在指定位置创建文件或设置 CHECK_CONFIG 指定你的文件。")
-        exit(1)
+    check_config = os.getenv("CHECK_CONFIG")
+    if check_config:
+        if not os.path.exists(check_config):
+            print(f"错误：环境变量 CHECK_CONFIG 指定的配置文件 {check_config} 不存在！")
+            exit(1)
+    else:
+        check_config = utils_env.get_file_path("check.toml")
+        if not check_config:
+            print("错误：未检查到签到配置文件，请在指定位置创建文件或设置 CHECK_CONFIG 指定你的文件。")
+            exit(1)
 
     try:
-        DATA = json.load(open(check_config, "r", encoding="utf-8"))
+        DATA = tomli.load(open(check_config, "rb"))
         return DATA
-    except ValueError:
+    except tomli.TOMLDecodeError:
         print(
-            f"错误：配置文件 {check_config} 格式不对，请在 https://verytoolz.com/json5-validator.html 中检查格式"
+            f"错误：配置文件 {check_config} 格式不对，请学习 https://toml.io/cn/v1.0.0\n错误信息：\n{traceback.format_exc()}"
         )
         exit(1)
