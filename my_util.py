4c685e8d4c280604612420ba80aec040eee14087
lustime <2247868358@qq.com>
2022-08-12 21:10:53 +0800

    bug fix.

--- my_util.py
+++ my_util.py
@@ -8,31 +8,23 @@ def get_user_id(emp_no: str) -> str:
     url = data.get('EMP_ID_URL')
 
     payload = {
-        "msname": "test",
-        "pageNo": 1,
-        "pageSize": 1,
-        "queryConditionDTO": {
-            "queryType": "Q0001",
-            "queryVer": "v1",
-            "queryKey": emp_no
+        "command": "TY-GR-002",
+        "params": {
+            "empShortId": emp_no
         }
 
     }
 
     headers = {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        'x-emp-no': emp_no,
+        'x-emp-id': '0',
+        'x-lang-id': 'zh_CN',
+        'x-tenant-id': '10000'
     }
 
     response = requests.request("POST", url, headers=headers, data=json.dumps(payload))
-    print(json.dumps(payload))
-    print(response)
-    print(response.text)
-    text = json.loads(response.text)
-    bo = text.get('bo')
-    rows = bo['rows']
-    result = rows[0]
-    return result['id']
-
+    return json.loads(response.text).get('bo').get('empId')
 
 def main():
     return get_user_id('XXX')
